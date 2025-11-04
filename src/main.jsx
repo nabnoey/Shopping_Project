b43b4c24f37fb92d8aace4468ac36d1fdc33f40a
nabnoey <nabnoeyy@gmail.com>
2025-11-04 15:44:53 +0700

    Edit main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Home from './Pages/Home'
+import { RouterProvider } from 'react-router-dom'
+import router from "./router/index.jsx"
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
